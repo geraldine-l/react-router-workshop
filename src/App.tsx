511286f44d36fbc294c59f72d4bcde9adce0279e
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-16 11:17:15 +0200

    add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "./App.css";
 
-// page components
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-
-// the App
-
 function App() {
-  const [currentLocation, setCurrentLocation] = useState("/");
-
   return (
     <>
       <nav>
-        <button onClick={() => setCurrentLocation("/")} type="button">
-          Home
-        </button>
-        <button onClick={() => setCurrentLocation("/about")} type="button">
-          About
-        </button>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </nav>
-      <main>
-        {currentLocation === "/" && <Home />}
-        {currentLocation === "/about" && <About />}
-      </main>
+      <main>???</main>
     </>
   );
 }
